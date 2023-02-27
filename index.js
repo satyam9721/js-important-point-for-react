383b194766eb850f1304abc9704484fd453ac1a1
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2023-02-27 12:39:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+//printing data with help for rest
+var students ={
+    name: "Ajay",
+    age:"28",
+    hobbies:["Cricket","Singing"]
+}
+const {age,...rest} = students;
 
-import reportWebVitals from './reportWebVitals';
+console.log(rest);
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//spread
+var newStudent={
+    ...students,
+  age: "23"  //overiding the value of age
+}
+console.log(newStudent);
